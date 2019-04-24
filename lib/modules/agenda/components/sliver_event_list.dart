749fd537059baf768a9ad7147ce27a52b5045f1c
Adson Leal <adsonpleal@gmail.com>
2019-04-24 14:57:21 -0300

    add padding to sliver list

--- lib/modules/agenda/components/sliver_event_list.dart
+++ lib/modules/agenda/components/sliver_event_list.dart
@@ -21,24 +21,32 @@ class SliverEventsList extends StatelessWidget {
       events,
       (Event e) => DateFormat.MMMd().format(e.date),
     );
+
+    List<Widget> slivers = group.keys.map(
+      (key) {
+        final events = group[key];
+        final date = events[0].date;
+        final weekDay = DateFormat.E().format(date).toUpperCase();
+        final formatedDate = DateFormat.MMMd().format(date);
+        return SliverStickyHeader(
+          overlapsContent: true,
+          header: AgendaHeader(
+            weekDay: weekDay,
+            formatedDate: formatedDate,
+          ),
+          sliver: EventList(events: events),
+        );
+      },
+    ).toList();
+    // Adds a bottom padding
+    slivers.add(SliverStickyHeader(
+      header: Container(
+        height: 100,
+      ),
+    ));
     return Flexible(
       child: CustomScrollView(
-        slivers: group.keys.map(
-          (key) {
-            final events = group[key];
-            final date = events[0].date;
-            final weekDay = DateFormat.E().format(date).toUpperCase();
-            final formatedDate = DateFormat.MMMd().format(date);
-            return SliverStickyHeader(
-              overlapsContent: true,
-              header: AgendaHeader(
-                weekDay: weekDay,
-                formatedDate: formatedDate,
-              ),
-              sliver: EventList(events: events),
-            );
-          },
-        ).toList(),
+        slivers: slivers,
       ),
     );
   }
